96e77ea7ae15d2f6bb2f9ccffac80181beba2e80
Muhammadamin1308 <sharifjanovmukhammadamin@gmail.com>
2022-07-04 00:55:58 -0700

    feat: some solves

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 module.exports = function reverse(num) {
-
-    let rev = 0;
-    let number1 = num;
-    let lastDigit;
-
-    while (number1 != 0) {
-        lastDigit = number1 % 10;
-        rev = rev * 10 + lastDigit;
-        number1 = Math.floor(number1 / 10);
-    }
-    return +rev
+    let toStr = '' + num
+    let sht = toStr.split("").reverse().join("");
+    return Number(sht)
 }
